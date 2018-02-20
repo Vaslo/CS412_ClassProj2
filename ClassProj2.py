d3a44e7cbe6c24c35e6790db61428ae306b49fd4
Vaslo <vaslo77@Gmail.com>
2018-02-19 22:55:17 -0500

    major updates to file, but still not getting credit

--- ClassProj2.py
+++ ClassProj2.py
@@ -1,6 +1,6 @@
+import pprint
 
-
-target = open("small_sample.txt")
+target = open("reviews_sample.txt")
 
 myList = []
 
@@ -14,77 +14,70 @@ def find_bigrams(input_list):
       bigram_list.append((input_list[i], input_list[i+1]))
   return bigram_list
 
-#print(myList)
+def find_trigrams(input_list):
+    trigram_list = []
+    for i in range(len(input_list)-2):
+        trigram_list.append((input_list[i], input_list[i+1], input_list[i+2]))
+    return trigram_list
+
 
 minSupport = 0.01
 support = int(minSupport*len(myList))
 
-newList = []
+biList = []
 for smallList in myList:
     bigrams = find_bigrams(smallList)
-    newList.append(bigrams)
-    
-print(newList)
+    biList.append(bigrams)
 
-#zipped = zip(myList, myList[1:])
+triList = []
+for smallList in myList:
+    trigrams = find_trigrams(smallList)
+    triList.append(trigrams)
 
-#print(list(zipped))
+biDict = {}
 
-input_list = ['all', 'this', 'happened', 'more', 'or', 'less']
+def addToDictionary(list):
+    tempDict = {}
+    for items in list:
+        for item in items:
+            tempDict.setdefault(item, 0)
+            tempDict[item] = tempDict[item] + 1
+    return tempDict
 
+biDict = addToDictionary(biList)
+triDict = addToDictionary(triList)
 
-#bigrams = find_bigrams(input_list)
-#print(bigrams)
+#print(masterDict)
 
-find_bigrams(input_list)
+bigramDict = {}
+bigramDict = { k:v for k, v in biDict.items() if v > 100 }
 
-#def find_trigrams(input_list):
-#  bigram_list = []
-#  for i in range(len(input_list)-2):
-#      bigram_list.append((input_list[i], input_list[i+1],input_list[i+2]))
-#  return bigram_list
-
-#trigrams = find_trigrams(input_list)
-#print(trigrams)
-
-find_bigrams(input_list)
-#def find_bigrams(input_list):
-#    for item in list:
-#        print(list(zip(input_list, input_list[1:])))
-
-#def find_bigrams(input_list):
-#    for item in list:
-#        print(list(item))
-     
-  
+trigramDict = {}
+trigramDict = { k:v for k, v in triDict.items() if v > 10 }
 
 
-#for list in myList:
-    #bigramList = zip(list,list[1:])
+#def printDict(dictionary):
+#    for key, value in dictionary.items():
+#        print(str(value)+":"+str(key))
 
-#print(list(bigramList))
+def printDictBi(dictionary):
+    for key, value in dictionary.items():
+            print(str(value)+":"+str(key[0])+";"+str(key[1]))
 
-
-    
-#ngram = zip(input_list, input_list[1:], input_list[2:])
-
-#x = [1,2,3]
-#y = [4,5,6]
-
-#zipped = zip(x,y)
-#zipped
+def printDictTri(dictionary):
+    for key, value in dictionary.items():
+            print(str(value)+":"+str(key[0])+";"+str(key[1]+";"+str(key[2])))
 
 
-#phrase1 = 'hoagie institution walking doe seem like throwback year ago old fashioned menu board booth large selection food speciality italian hoagie voted best area year year usually order burger patty obviously cooked frozen ingredient fresh overall good alternative subway road'
-#phrase2 = 'excellent food superb customer service miss mario machine used still great place steeped tradition'
+#printDictBi(bigramDict)
+printDictTri(trigramDict)
 
-#fixedPhrase = phrase1.split()
-#print(fixedPhrase)
+def writeDictBi(dictionary,fileName):
+    file = open(fileName, 'w')
+    for key, value in dictionary.items():
+        file.write(str(value)+":"+str(key[0])+";"+str(key[1])+"\n")
+    file.close()
 
-#bigram = find_bigrams(myList)
-#find_bigrams(myList)
-#unigram = find_unigrams(fixedPhrase)
-#print(list(bigram))
-#print(list(unigram))
+writeDictBi(bigramDict,'output.txt')
 
 
